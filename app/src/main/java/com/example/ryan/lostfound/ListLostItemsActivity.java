2d39d406ccd74e473a44c501d5f5dc7df6e2c185
huygaa11 <batbatsaikhan@gmail.com>
2015-11-07 13:31:19 -0500

    added everything with maps and lists

--- app/src/main/java/com/example/ryan/lostfound/ListLostItemsActivity.java
+++ app/src/main/java/com/example/ryan/lostfound/ListLostItemsActivity.java
@@ -1,50 +1,133 @@
 package com.example.ryan.lostfound;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 
 public class ListLostItemsActivity extends AppCompatActivity {
 
+    ArrayList<String> titles;
+    ListView listview;
+    ArrayAdapter adapter;
+    JSONArray jsonArray;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_lost_items);
 
-
-
-
-
-        final ListView listview = (ListView) findViewById(R.id.lostItemListView);
-        String[] values = new String[] { "Android", "iPhone", "WindowsMobile",
-                "Blackberry", "WebOS", "Ubuntu", "Windows7", "Max OS X",
-                "Linux", "OS/2", "Ubuntu", "Windows7", "Max OS X", "Linux",
-                "OS/2", "Ubuntu", "Windows7", "Max OS X", "Linux", "OS/2",
-                "Android", "iPhone", "WindowsMobile" };
-
-        final ArrayList<String> list = new ArrayList<String>();
-        for (int i = 0; i < values.length; ++i) {
-            list.add(values[i]);
-        }
-        final ArrayAdapter adapter = new ArrayAdapter(this,
-                android.R.layout.simple_list_item_1, list);
-        listview.setAdapter(adapter);
+        listview = (ListView) findViewById(R.id.lostItemListView);
+        titles = new ArrayList<>();
 
         listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
 
             @Override
             public void onItemClick(AdapterView<?> parent, final View view,
                                     int position, long id) {
-                final String item = (String) parent.getItemAtPosition(position);
+                String item = "bad";
 
-                list.remove(item);
-                adapter.notifyDataSetChanged();
+                try {
+                    item = jsonArray.getJSONObject(position).getString("_id");
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+                // list.remove(item);
+                // adapter.notifyDataSetChanged();
+
+
+                Toast.makeText(ListLostItemsActivity.this, item, Toast.LENGTH_SHORT).show();
+                goToMap(item);
             }
         });
     }
+
+    @Override
+    protected void onStart() {
+
+        super.onStart();
+        getList();
+        renderList();
+    }
+
+    private void getList() {
+        // getWebsite();
+        // Log.d("MESSAGE", "STARTED");
+
+        final TextView mTextView = (TextView) findViewById(R.id.output);
+
+        // Instantiate the RequestQueue.
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url ="http://72.19.65.87:3000/lost";
+
+        // Request a string response from the provided URL.
+        JsonObjectRequest stringRequest = new JsonObjectRequest(Request.Method.GET, url, null,
+                new Response.Listener<JSONObject>() {
+
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        // Display the first 500 characters of the response string.
+                        try {
+                            jsonArray = response.getJSONArray("items");
+
+                            for (int i = 0; i < jsonArray.length(); i++) {
+                                JSONObject json = response.getJSONArray("items").getJSONObject(i);
+                                titles.add(json.getString("title"));
+                            }
+
+                            adapter.notifyDataSetChanged();
+                            // mTextView.setText(response.getJSONArray("items").toString());
+                        } catch (Exception e) {
+
+                        }
+
+
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                mTextView.setText("That didn't work!");
+            }
+        });
+        // Add the request to the RequestQueue.
+        queue.add(stringRequest);
+    }
+
+
+    private void goToMap(String id) {
+        Intent mapIntent = new Intent(this, SearchMapsActivity.class);
+        mapIntent.putExtra("id", id);
+        startActivityForResult(mapIntent, 1);
+    }
+
+
+    private void renderList() {
+        // titles.add("hello");
+
+        adapter = new ArrayAdapter(this,
+                android.R.layout.simple_list_item_1, titles);
+        listview.setAdapter(adapter);
+
+    }
+
 }
+
